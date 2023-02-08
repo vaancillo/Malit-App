e4fb6ee75eff9e82e5b54049272bc26c711c5224
vancillo <yaredbane@gmail.com>
2023-02-08 16:24:16 -0600

    Changes

---
+++ src/assets/index.js
@@ -0,0 +1,24 @@
+
+import logo from "./logo.svg";
+import star from "./Star.svg";
+import shield from "./shield.svg"
+import menu from "./menu.svg";
+import close from "./close.svg";
+import instagram from "./instagram.svg";
+import exampleMenu from "./exampleMenu.png";
+import exampleMenu2 from "./exampleMenu2.png";
+import menus from "./menus.png"
+import malit from "./MalitLogo.png"
+
+export {
+  malit,
+  menus,
+  exampleMenu2,
+  exampleMenu,
+  logo,
+  shield,
+  star,
+  menu,
+  close, 
+  instagram,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,58 @@
+import { instagram, star, shield  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "inicio",
+    title: "Inicio",
+  },
+  {
+    id: "servicio",
+    title: "Servicio",
+  },
+  {
+    id: "soluciones",
+    title: "Soluciones",
+  },
+  {
+    id: "nosotros",
+    title: "Nosotros",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Calidad",
+    content:
+      "Obtén la mejor calidad a precios asequibles en comparación con nuestros competidores.",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "Confianza",
+    content:
+      "MALIT se enorgullece de garantizar cumplimiento y brindar un servicio al cliente de alta calidad."
+  },
+];
+
+
+
+export const stats = [
+  {
+    id: "stats-2",
+    title: "Restaurantes y fondas confian en nosotros",
+    value: "30+",
+  }
+];
+
+
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://instagram.com/malitservice?igshid=YmMyMTA2M2Y=",
+  },
+];
+
